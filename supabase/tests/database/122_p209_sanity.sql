81062732b350cad1761e92836cbc5a58c769e88e
imranmirzadubai-dot <imranmirzadubai@gmail.com>
2026-09-20 09:38:58 +0400

    chore: remove redundant P14-T209 sanity test

--- supabase/tests/database/122_p209_sanity.sql
+++
@@ -1,5 +0,0 @@
-begin;
-select plan(1);
-select ok(exists (select 1 from pg_proc where proname='claim_command_idempotency'), 'P14-T209 idempotency command exists');
-select * from finish();
-rollback;
