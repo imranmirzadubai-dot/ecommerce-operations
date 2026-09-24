76b1c378421489dc6680e439d6b462abd6fa3ff2
imranmirzadubai-dot <imranmirzadubai@gmail.com>
2026-09-24 12:31:59 +0400

    fix UI-002 remove unused effect import

--- src/components/OrderBatchSelection.tsx
+++ src/components/OrderBatchSelection.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import { OrdersWorkspace } from './OrdersWorkspace'
 import type { OrderListRow } from '../lib/commands'
 import { OperationalStatusIndicators } from './OperationalStatusIndicators'
